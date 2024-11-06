b53c8b03d9b62dcf123887ae9c039f988698df49
Madiyar08 <abdusamatovmadiar@gmail.com>
2024-11-06 14:56:38 +0500

    settings is updated

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Путь, где буду
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2hn0&ye6sp_-ietg1@y*!-()+dce!=)q*fo#5_x3y+xo-)-ns7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -91,7 +91,7 @@ DATABASES = {
     }
 }
 
-
+database_url = os.environ.get("DATABASE_URL")
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
