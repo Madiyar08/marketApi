bd51674ed9e767e298e92fce3ecd6f7b40fb1154
Madiyar08 <abdusamatovmadiar@gmail.com>
2026-02-06 17:34:59 +0500

    Apply database migrations and fix admin URLs

--- markets/urls.py
+++ markets/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from markets.views import MarketViewSet, handle_text_data
@@ -8,7 +8,6 @@ router = DefaultRouter()
 router.register(r'markets', MarketViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('login/', login_view, name='login'),
     path('api/', include(router.urls)),  # Подключаем маршруты API
     path('markets/process-market-data/', handle_text_data, name='process-market-data'),  # Обработчик текстовых данных
