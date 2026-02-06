da6cbca123753a391df99393bb46ad87227c3ec5
Madiyar08 <abdusamatovmadiar@gmail.com>
2026-02-06 17:34:46 +0500

    Ignore environment variables

--- config/settings.py
+++ config/settings.py
@@ -84,14 +84,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
 database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    # Используем Neon PostgreSQL
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=database_url,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    # Для локальной разработки используем SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
